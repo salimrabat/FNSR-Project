b54b3c34b6df83e6223036afc25ed3f4ff807b70
salimrabat <rabat@kth.se>
2017-05-07 15:29:56 +0200

    Changed from javafx to LibGDX, Code Skeletons added

---
+++ desktop/src/com/projinda/fnsr/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.projinda.fnsr.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.projinda.fnsr.UnnamedGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UnnamedGame(), config);
+	}
+}
